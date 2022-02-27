d441426bfc89862deb5ed29d735e30225559ba96
mailalexjoseph <89794226+mailalexjoseph@users.noreply.github.com>
2022-02-27 10:20:32 +0530

    Script exercise2

---
+++ 05.Lesson_GettingFamiliarWithCVT/ScriptExercise2/MeetingSpecRule.sh
@@ -0,0 +1,1 @@
+certoraRun MeetingSchedulerFixed.sol:MeetingScheduler --verify MeetingScheduler:meetings.spec --solc solc8.7 --rule startOnTime --msg 'for all methods'
---
+++ 05.Lesson_GettingFamiliarWithCVT/ScriptExercise2/Meetingspec.sh
@@ -0,0 +1,1 @@
+certoraRun MeetingSchedulerFixed.sol:MeetingScheduler --verify MeetingScheduler:meetings.spec --solc solc8.7 --msg 'Verfication of meetings spec' --send_only
